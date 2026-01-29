5eb575eb2d9045d1357b02699ff3873227e931fd
Jori Vehkakoski <j.vehkakoski@gmail.com>
2026-01-29 18:37:39 +0200

    Sign in view and navigation

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,14 +1,17 @@
 import { View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import Text from './Text';
+import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#24292e',
+    flexDirection: 'row',
   },
   title: {
-    color: 'white'
+    color: 'white',
+    padding: 10
   }
 
 });
@@ -17,7 +20,14 @@ const AppBar = () => {
   return (
     <View style={styles.container}>
       <Pressable>      
-        <Text style={styles.title}>Repositories</Text>
+        <Link to="/">
+          <Text style={styles.title}>Repositories</Text>
+        </Link>
+      </Pressable>
+      <Pressable>
+        <Link to="/signin">
+          <Text style={styles.title}>Sign in</Text> 
+        </Link>
       </Pressable>
     </View>
   );
