fdf80d3724646f887901090a35fce5fb772b0f87
Jori Vehkakoski <j.vehkakoski@gmail.com>
2026-02-03 01:38:08 +0200

    graphql first steps

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
+
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    // Replace the IP address part with your own IP address!
+    const response = await fetch('http://192.168.0.58:5001/api/repositories');
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
